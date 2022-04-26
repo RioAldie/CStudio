9e7e56d7580be41090397baba91501339d4487c0
rioaldie <rioaldierwanto@gmail.com>
2022-04-26 23:53:47 +0700

    add more post

--- src/component/feed.tsx
+++ src/component/feed.tsx
@@ -21,6 +21,9 @@ const Feed = () =>{
             <PostContainer >
                 <Post name="Alexandra Mitrova" image="/image/david-clode-uTj6-eZeHto-unsplash.jpg" date="September 14, 2016" avatar="/image/avatar-1.png"/>
                 <Post name="Teressa Rogers" image="/image/alvan-nee-ZCHj_2lJP00-unsplash.jpg" date="January 19, 2022" avatar="/image/avatar-5.png"/>
+                <Post name="Sean Dong" image="/image/moment-beach.jpg" date="Desember 29, 2021" avatar="/image/avatar-2.png"/>
+                <Post name="Rudi Santoso" image="/image/nature-vibes.jpg" date="Maret 10, 2022" avatar="/image/avatar-4.png"/>
+                <Post name="Sakamoto Rina" image="/image/wedding-cake.jpg" date="April 15, 2022" avatar="/image/avatar-3.png"/>
             </PostContainer>
         
         </Box>
